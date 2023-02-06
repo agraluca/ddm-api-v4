f0fce5a74186113fa29dd9d869fd4cedce6f1f54
Luca Agra <luca.agra@hotmail.com>
2023-02-06 17:43:19 -0300

    fix: adding config for ckeditor5

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,8 @@
 module.exports = ({ env }) => ({
+  ckeditor5: {
+    enabled: true,
+    resolve: "./src/plugins/strapi-plugin-ckeditor",
+  },
   email: {
     provider: "nodemailer",
     providerOptions: {
