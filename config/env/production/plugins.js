9212409437683c349c6d0a9088c23bfb4c39a37d
Luca Agra <luca.agra@hotmail.com>
2023-02-08 10:17:22 -0300

    fix: adding config to nodemailer to prod

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,19 @@
 module.exports = ({ env }) => ({
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
+      port: env("SMTP_PORT", 587),
+      auth: {
+        user: env("SMTP_USERNAME"),
+        pass: env("SMTP_PASSWORD"),
+      },
+    },
+    settings: {
+      defaultFrom: "joao.marinho@diariodomilhao.com",
+      defaultReplyTo: "joao.marinho@diariodomilhao.com",
+    },
+  },
   upload: {
     config: {
       provider: "cloudinary",
--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,5 @@
 module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
-        port: env("SMTP_PORT", 587),
-        auth: {
-          user: env("SMTP_USERNAME"),
-          pass: env("SMTP_PASSWORD"),
-        },
-      },
+
       settings: {
         defaultFrom: "joao.marinho@diariodomilhao.com",
         defaultReplyTo: "joao.marinho@diariodomilhao.com",
