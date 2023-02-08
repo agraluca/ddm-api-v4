348806ee52acc77925c53bee401efcdbbb471325
Luca Agra <luca.agra@hotmail.com>
2023-02-08 11:00:03 -0300

    fix: removing nodemailer to check if he it's the problem

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,21 @@
 module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
-        port: env("SMTP_PORT", 587),
-        auth: {
-          user: env("SMTP_USERNAME"),
-          pass: env("SMTP_PASSWORD"),
-        },
-      },
-      settings: {
-        defaultFrom: "joao.marinho@diariodomilhao.com",
-        defaultReplyTo: "joao.marinho@diariodomilhao.com",
-      },
-    },
-  },
+  // email: {
+  //   config: {
+  //     provider: "nodemailer",
+  //     providerOptions: {
+  //       host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
+  //       port: env("SMTP_PORT", 587),
+  //       auth: {
+  //         user: env("SMTP_USERNAME"),
+  //         pass: env("SMTP_PASSWORD"),
+  //       },
+  //     },
+  //     settings: {
+  //       defaultFrom: "joao.marinho@diariodomilhao.com",
+  //       defaultReplyTo: "joao.marinho@diariodomilhao.com",
+  //     },
+  //   },
+  // },
   upload: {
     config: {
       provider: "cloudinary",
