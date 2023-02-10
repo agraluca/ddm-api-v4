77349c8d529e77c683eaeede602cab0aabbfa3ef
Luca Agra <luca.agra@hotmail.com>
2023-02-10 10:30:09 -0300

    chore: populate permission on login

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,125 @@
+// src/extensions/users-permissions/strapi-server.js
+
+const _ = require("lodash");
+const {
+  validateCallbackBody,
+} = require("@strapi/plugin-users-permissions/server/controllers/validation/auth");
+
+const { getService } = require("@strapi/plugin-users-permissions/server/utils");
+const { sanitize } = require("@strapi/utils");
+
+module.exports = (plugin) => {
+  //sanitize-function
+  const sanitizeUser = (user, ctx) => {
+    const { auth } = ctx.state;
+    const userSchema = strapi.getModel("plugin::users-permissions.user");
+    return sanitize.contentAPI.output(user, userSchema, { auth });
+  };
+  // edit-callback-on-login
+  plugin.controllers.auth.callback = async (ctx) => {
+    const emailRegExp =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const provider = ctx.params.provider || "local";
+    const params = ctx.request.body;
+    const store = await strapi.store({
+      type: "plugin",
+      name: "users-permissions",
+    });
+
+    if (provider === "local") {
+      if (!_.get(await store.get({ key: "grant" }), "email.enabled")) {
+        throw new ApplicationError("This provider is disabled");
+      }
+
+      await validateCallbackBody(params);
+
+      const query = { provider };
+
+      // Check if the provided identifier is an email or not.
+      const isEmail = emailRegExp.test(params.identifier);
+
+      // Set the identifier to the appropriate query field.
+      if (isEmail) {
+        query.email = params.identifier.toLowerCase();
+      } else {
+        query.username = params.identifier;
+      }
+
+      // Check if the user exists.
+      //populate any field you wnat in my case avatar is the field which I want to populate.
+      const user = await strapi
+        .query("plugin::users-permissions.user")
+        .findOne({ where: query, populate: ["coursePermission"] });
+
+      if (!user) {
+        throw new ValidationError("Invalid identifier or password");
+      }
+
+      if (
+        _.get(await store.get({ key: "advanced" }), "email_confirmation") &&
+        user.confirmed !== true
+      ) {
+        throw new ApplicationError("Your account email is not confirmed");
+      }
+
+      if (user.blocked === true) {
+        throw new ApplicationError(
+          "Your account has been blocked by an administrator"
+        );
+      }
+
+      // The user never authenticated with the `local` provider.
+      if (!user.password) {
+        throw new ApplicationError(
+          "This user never set a local password, please login with the provider used during account creation"
+        );
+      }
+
+      const validPassword = await getService("user").validatePassword(
+        params.password,
+        user.password
+      );
+
+      if (!validPassword) {
+        throw new ValidationError("Invalid identifier or password");
+      } else {
+        console.log("Sanitized", await sanitizeUser(user, ctx));
+
+        ctx.send({
+          jwt: getService("jwt").issue({
+            id: user.id,
+          }),
+          user: await sanitizeUser(user, ctx),
+        });
+      }
+    } else {
+      if (!_.get(await store.get({ key: "grant" }), [provider, "enabled"])) {
+        throw new ApplicationError("This provider is disabled");
+      }
+
+      // Connect the user with the third-party provider.
+      let user;
+      let error;
+      try {
+        [user, error] = await getService("providers").connect(
+          provider,
+          ctx.query
+        );
+      } catch ([user, error]) {
+        throw new ApplicationError(error.message);
+      }
+
+      if (!user) {
+        throw new ApplicationError(error.message);
+      }
+      const explore = await sanitizeUser(user, ctx);
+      ctx.send({
+        jwt: getService("jwt").issue({ id: user.id }),
+        user: explore,
+      });
+    }
+  };
+
+  return plugin;
+};
